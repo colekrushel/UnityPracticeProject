5a8a43abe9e477e15785ee0511f4772c3ba46868
colekrushel <krushc@rpi.edu>
2024-11-08 19:48:30 -0500

    more teleport system code and adjusted scene

--- Assets/character/playerInteractionCollider.cs
+++ Assets/character/playerInteractionCollider.cs
@@ -11,10 +11,15 @@ public class playerInteractionCollider : MonoBehaviour
     GameObject indicator;
     GameObject textbox;
     GameObject player;
+    [SerializeField] GameObject destinationRoom;
     Collider playerCollider;
     Dictionary<string, string> desc;
     Dictionary<string, Vector3> locs;
-    Dictionary<string, string> scenes;
+    Dictionary<string, string> roomLocs;
+    Dictionary<string, GameObject> rooms;
+    Dictionary<string, string> loadZones;
+    Dictionary<string, string> chunkDirs;
+    string enteredFrom;
     void Start()
     {
         //initialize objects
@@ -22,12 +27,16 @@ public class playerInteractionCollider : MonoBehaviour
         player = gameObject;
         indicator = GameObject.Find("indicator");
         textbox = GameObject.Find("inspectTextbox");
+        destinationRoom = GameObject.Find("DefaultRoom");
         //set textbox deactive by default
         textbox.SetActive(false);
         playerCollider = gameObject.GetComponent<Collider>();
         desc = objectInfo.objectDescs;
         locs = objectInfo.teleLocations;
-        scenes = objectInfo.sceneLocations;
+        roomLocs = objectInfo.roomLocations;
+        rooms = objectInfo.rooms;
+        loadZones = objectInfo.chunkTriggers;
+        chunkDirs = objectInfo.chunkSides;
         //ignore collision between player and this collision box
         //Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), player.GetComponent<Collider>());
 
@@ -77,21 +86,69 @@ public class playerInteractionCollider : MonoBehaviour
         {
             collisionObject = other.gameObject;
         }
-        if (other.tag == "Teleporter")
+        if (other.tag == "Transporter")
         {
             print("tele");
             //move player according to the teleporter's location in the dictionary
-            //need to disable player controller in order to teleport
-            //player.GetComponent<CharacterController>().enabled = false;
-            //player.transform.position = locs[other.name];
-            //player.GetComponent<CharacterController>().enabled = true;
+            //disable prev room
+            destinationRoom.SetActive(false);
+            destinationRoom = rooms[roomLocs[other.name]];
+            //enable room player is teleporting to
+            destinationRoom.SetActive(true);
 
-            //change the scene
-            string sceneName = scenes[other.name];
-            SceneManager.LoadScene(sceneName);
+            //need to disable player controller in order to teleport
+            player.GetComponent<CharacterController>().enabled = false;
+            player.transform.position = locs[other.name];
+            player.GetComponent<CharacterController>().enabled = true;
+
+            //disable indicator
+            indicator.transform.position = new Vector3(0, 0, 0);
+            //disable textbox
+            textbox.SetActive(false);
 
         }
-        print(other.tag);
+        if (other.tag == "Teleporter")
+        {
+            //need to disable player controller in order to teleport
+            player.GetComponent<CharacterController>().enabled = false;
+            player.transform.position = locs[other.name];
+            player.GetComponent<CharacterController>().enabled = true;
+
+            //disable indicator
+            indicator.transform.position = new Vector3(0, 0, 0);
+            //disable textbox
+            textbox.SetActive(false);
+        }
+        if (other.tag == "ChunkTrigger")
+        {
+            //load in chunk
+            //get direction
+            string direction = chunkDirs[other.name];
+            //load prefab
+            GameObject newChunk = rooms[loadZones[other.name]];
+            //discriminate based on direction
+            if (direction == "R")
+            {
+                //instantiate prefab to the right of the load zone
+                Instantiate(newChunk, newChunk.transform.position, Quaternion.identity);
+                //move prefab
+                newChunk.transform.position = other.transform.position + new Vector3(33, 2, -2);
+                //disable this loader as chunk is now loaded
+                other.gameObject.SetActive(false);
+                //disable the left loader on the new chunk
+                //GameObject leftLoader = newChunk.Find("LeftLoader");
+            }
+            else if (direction == "L")
+            {
+                Instantiate(newChunk, newChunk.transform.position, Quaternion.identity);
+                //move prefab
+                newChunk.transform.position = other.transform.position - new Vector3(33, -2, 2);
+                //disable this loader as chunk is now loaded
+                other.gameObject.SetActive(false);
+                //disable the right loader on the new chunk
+            }
+            print(other.tag);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
--- Assets/helpers/objectInfo.cs
+++ Assets/helpers/objectInfo.cs
@@ -11,14 +11,36 @@ public class objectInfo
     };
     public static Dictionary<string, Vector3> teleLocations = new Dictionary<string, Vector3>()
     {
-        { "PyramidArch", new Vector3(0, 0, 0)  }
-        
-    };
-    public static Dictionary<string, string> sceneLocations = new Dictionary<string, string>()
-    {
-        { "PyramidArch", "PyramidScene" },
-        { "PyramidDoor", "SampleScene" }
+        { "PyramidArch", new Vector3(-51, -3, 42)  },
+        { "PyramidDoor", new Vector3(0, 0, 0) },
+        { "RightDoor", new Vector3(169-58, -2, 62) },
+        { "LeftDoor", new Vector3(169, -2, 62) },
+        { "StreetArch", new Vector3(126, -2, 62) }
 
+    };
+    public static Dictionary<string, string> roomLocations = new Dictionary<string, string>()
+    {
+        { "PyramidArch", "Room1" },
+        { "PyramidDoor", "DefaultRoom" },
+        { "StreetArch", "Room2" }
+
+    };
+    public static Dictionary<string, string> chunkTriggers = new Dictionary<string, string>()
+    {
+        { "RightDoor", "Room2" },
+        { "LeftDoor", "Room2" }
+    };
+    public static Dictionary<string, string> chunkSides = new Dictionary<string, string>()
+    {
+        { "RightDoor", "R" },
+        { "LeftDoor", "L" }
+    };
+    //references to all of the room game objects
+    public static Dictionary<string, GameObject> rooms = new Dictionary<string, GameObject>()
+    {
+        {"DefaultRoom", GameObject.Find("DefaultRoom") },
+        {"Room1", GameObject.Find("Room1") },
+        {"Room2", GameObject.Find("Room2") }
     };
 
 
